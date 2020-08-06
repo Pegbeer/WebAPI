3ce801587512fc583f769539a5a233cb719797d5
Carlos <55509957+Pegbeer@users.noreply.github.com>
2020-08-06 08:23:39 -0600

    Agregar archivos de proyecto.

---
+++ Models/PruebaXamarinBDContext.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebAPI.Models
+{
+    public partial class PruebaXamarinBDContext : DbContext
+    {
+        public PruebaXamarinBDContext()
+        {
+        }
+
+        public PruebaXamarinBDContext(DbContextOptions<PruebaXamarinBDContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Productos> Productos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=tcp:pruebaxamarinbddbserver.database.windows.net;Initial Catalog=PruebaXamarinBD;User ID=Carlos;Password=MetalK1ller;Connect Timeout=60;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Productos>(entity =>
+            {
+                entity.ToTable("productos");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Cantidad).HasColumnName("cantidad");
+
+                entity.Property(e => e.CodBarras).HasColumnName("codBarras");
+
+                entity.Property(e => e.Descripcion)
+                    .HasColumnName("descripcion")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Img).HasColumnName("img");
+
+                entity.Property(e => e.Nombre)
+                    .HasColumnName("nombre")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
